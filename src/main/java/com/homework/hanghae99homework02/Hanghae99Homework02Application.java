514e8340b9d0ecc96e3b46cbb7abe98cdac693fb
ojy9612 <pvnm@naver.com>
2022-06-21 14:29:36 +0900

    S3 서버 다운 테스트 코드 수정 (ci/cd 진행중)

--- src/main/java/com/homework/hanghae99homework02/Hanghae99Homework02Application.java
+++ src/main/java/com/homework/hanghae99homework02/Hanghae99Homework02Application.java
@@ -2,6 +2,7 @@ package com.homework.hanghae99homework02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.annotation.PostConstruct;
@@ -9,6 +10,7 @@ import java.util.TimeZone;
 
 @EnableJpaAuditing //?? 어떻게 바꿔야하지? 에러가능성 TODO
 @SpringBootApplication
+@PropertySource("application-dev.properties")
 public class Hanghae99Homework02Application {
 
     public static void main(String[] args) {
