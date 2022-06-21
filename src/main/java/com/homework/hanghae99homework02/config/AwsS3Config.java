ed6dcfd3d76b401fea1de081effff6295201ae4a
ojy9612 <pvnm@naver.com>
2022-06-21 14:18:10 +0900

    S3 서버 다운 테스트 코드 수정 (ci/cd 진행중)

--- src/main/java/com/homework/hanghae99homework02/config/AwsS3Config.java
+++ src/main/java/com/homework/hanghae99homework02/config/AwsS3Config.java
@@ -1,4 +1,4 @@
-package com.homework.hanghae99homework02.s3;
+package com.homework.hanghae99homework02.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
